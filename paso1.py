6f2a77263136a85beda853581f21208fcc56c8f2
Fernando Acuna <facunac@gmail.com>
2017-10-13 16:43:27 -0300

    primer commit
    demos

---
+++ paso1.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import pymongo
+
+client = pymongo.MongoClient("mongodb://dec_user:PJoafIu0M01M5gzI@decdev-shard-00-00-fzh1i.mongodb.net:27017,decdev-shard-00-01-fzh1i.mongodb.net:27017,decdev-shard-00-02-fzh1i.mongodb.net:27017/test?ssl=true&replicaSet=decdev-shard-0&authSource=admin")
+
+db = client.test
+
+col = db.users
+
+col.find_one({"email": "itzel@hyatt.co"})
+
+print col
